6f8f7079f07be63f4ba6d985071688b198ec46e0
Phuoc Dinh <pdinh4396@gmail.com>
2025-09-13 17:05:39 +0700

    Add function to calculate age from birth year

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ function showInfo(name, age) {
     return `Tên: ${name}, Tuổi: ${age}`;
 }
 
+// Hàm tính tuổi từ năm sinh
+function calcAge(name, birthYear) {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - birthYear;
+    return showInfo(name, age);
+}
+
 // In kết quả ra màn hình
-console.log(showInfo("Phuoc", 21));
+console.log(calcAge("Phuoc", 2004));
